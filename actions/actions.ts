48081c0d5cd79bc867c367ef3c0dec870afff114
lucidiusss <cheapp78@gmail.com>
2024-12-27 21:56:53 +0300

    upd search functionality

--- actions/actions.ts
+++ actions/actions.ts
@@ -8,83 +8,11 @@ export const getOngoingAnimes = async () => {
   const query = `
   query {
     animes(limit: 28, status: "ongoing", order: ranked) {
-    id  
-    malId
     name
-    russian
-    licenseNameRu
-    english
-    japanese
-    synonyms
-    kind
-    rating
-    score
-    status
-    episodes
-    episodesAired
-    duration
-    airedOn { year month day date }
-    releasedOn { year month day date }
-    url
-    season
-
     poster { id originalUrl mainUrl }
-
-    fansubbers
-    fandubbers
-    licensors
-    createdAt,
-    updatedAt,
-    nextEpisodeAt,
-    isCensored
-
-    genres { id name russian kind }
+    id
+    url
     studios { id name imageUrl }
-
-    externalLinks {
-      id
-      kind
-      url
-      createdAt
-      updatedAt
-    }
-
-    personRoles {
-      id
-      rolesRu
-      rolesEn
-      person { id name poster { id } }
-    }
-    characterRoles {
-      id
-      rolesRu
-      rolesEn
-      character { id name poster { id } }
-    }
-
-    related {
-      id
-      anime {
-        id
-        name
-      }
-      manga {
-        id
-        name
-      }
-      relationKind
-      relationText
-    }
-
-    videos { id url name kind playerUrl imageUrl }
-    screenshots { id originalUrl x166Url x332Url }
-
-    scoresStats { score count }
-    statusesStats { status count }
-
-    description
-    descriptionHtml
-    descriptionSource
   }
   }
   `;
@@ -104,83 +32,13 @@ export const getAnimes = async () => {
   const query = `
   query {
     animes(limit: 28, order: ranked) {
+    name
+    poster { id originalUrl mainUrl }
     id
-    malId
+    url
+    studios { id name imageUrl }
     name
     russian
-    licenseNameRu
-    english
-    japanese
-    synonyms
-    kind
-    rating
-    score
-    status
-    episodes
-    episodesAired
-    duration
-    airedOn { year month day date }
-    releasedOn { year month day date }
-    url
-    season
-
-    poster { id originalUrl mainUrl }
-
-    fansubbers
-    fandubbers
-    licensors
-    createdAt,
-    updatedAt,
-    nextEpisodeAt,
-    isCensored
-
-    genres { id name russian kind }
-    studios { id name imageUrl }
-
-    externalLinks {
-      id
-      kind
-      url
-      createdAt
-      updatedAt
-    }
-
-    personRoles {
-      id
-      rolesRu
-      rolesEn
-      person { id name poster { id } }
-    }
-    characterRoles {
-      id
-      rolesRu
-      rolesEn
-      character { id name poster { id } }
-    }
-
-    related {
-      id
-      anime {
-        id
-        name
-      }
-      manga {
-        id
-        name
-      }
-      relationKind
-      relationText
-    }
-
-    videos { id url name kind playerUrl imageUrl }
-    screenshots { id originalUrl x166Url x332Url }
-
-    scoresStats { score count }
-    statusesStats { status count }
-
-    description
-    descriptionHtml
-    descriptionSource
   }
   }
   `;
@@ -298,84 +156,14 @@ export const searchAnimes = async (name: string) => {
   const query = `
 
   query {
-    animes(search: "${name}", limit: 100) {
+    animes(search: "${name}", limit: 50) {
+    name
+    poster { id originalUrl mainUrl }
     id
-    malId
+    url
     name
     russian
-    licenseNameRu
-    english
-    japanese
-    synonyms
-    kind
-    rating
-    score
-    status
-    episodes
-    episodesAired
-    duration
-    airedOn { year month day date }
-    releasedOn { year month day date }
-    url
-    season
-
-    poster { id originalUrl mainUrl }
-
-    fansubbers
-    fandubbers
-    licensors
-    createdAt,
-    updatedAt,
-    nextEpisodeAt,
-    isCensored
-
-    genres { id name russian kind }
     studios { id name imageUrl }
-
-    externalLinks {
-      id
-      kind
-      url
-      createdAt
-      updatedAt
-    }
-
-    personRoles {
-      id
-      rolesRu
-      rolesEn
-      person { id name poster { id } }
-    }
-    characterRoles {
-      id
-      rolesRu
-      rolesEn
-      character { id name poster { id } }
-    }
-
-    related {
-      id
-      anime {
-        id
-        name
-      }
-      manga {
-        id
-        name
-      }
-      relationKind
-      relationText
-    }
-
-    videos { id url name kind playerUrl imageUrl }
-    screenshots { id originalUrl x166Url x332Url }
-
-    scoresStats { score count }
-    statusesStats { status count }
-
-    description
-    descriptionHtml
-    descriptionSource
   }
   }
   `;
